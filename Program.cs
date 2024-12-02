623dd79f85266cf92cc76446485aae883fd76404
rmontecino <rmontecino.net@gmail.com>
2024-12-02 15:38:57 -0300

    Adding sorted squares

--- Program.cs
+++ Program.cs
@@ -4,8 +4,16 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        // Test the palindrome method
-        string s = "racecar";
-        Console.WriteLine(TwoPointers.CheckIfPalindrome(s));
+        // // Test the palindrome method
+        // string s = "racecar";
+        // Console.WriteLine(TwoPointers.CheckIfPalindrome(s));
+
+        // Test the sorted squares method
+        int[] arr = new int[] { -4, -1, 0, 3, 10 };
+        int[] result = TwoPointers.SortedSquares(arr);
+        foreach (var item in result)
+        {
+            Console.WriteLine(item);
+        }
     }
 }
--- code-challenges/array-and-strings/TwoPointers.cs
+++ code-challenges/array-and-strings/TwoPointers.cs
@@ -28,12 +28,43 @@ namespace CodeChallenges.ArrayAndStrings
             while (left < right)
             {
                 if (s[left] != s[right])
+                {
                     return false;
+                }
                 left++;
                 right--;
             }
 
             return true;
+        }
+
+        public static int[] SortedSquares(int[] arr)
+        {
+            int n = arr.Length;
+            int left = 0;
+            int right = n - 1;
+            int position = n - 1;
+            int[] result = new int[n];
+
+            while (left <= right)
+            {
+                int leftSquare = arr[left] * arr[left];
+                int rightSquare = arr[right] * arr[right];
+
+                if (leftSquare < rightSquare)
+                {
+                    result[position] = rightSquare;
+                    right--;
+                }
+                else
+                {
+                    result[position] = leftSquare;
+                    left++;
+                }
+                position--;
+            }
+
+            return result;
         }
     }
 }
