7a3faeadb9dedc92265cfe8d09c902386938d926
rmontecino <rmontecino.net@gmail.com>
2024-12-13 20:43:55 -0300

    Adding hashmaps exercises

---
+++ code-challenges-test/HashMapsTests.cs
@@ -0,0 +1,74 @@
+﻿using Xunit;
+
+namespace CodeChallenges.Tests
+{
+    public class HashMapsTests
+    {
+        [Fact]
+        public void TwoSum_ReturnsCorrectIndices()
+        {
+            // Arrange
+            int[] nums = { 2, 7, 11, 15 };
+            int target = 9;
+
+            // Act
+            int[] result = HashMaps.HashMaps.TwoSum(nums, target);
+
+            // Assert
+            Assert.Equal(new int[] { 0, 1 }, result);
+        }
+
+        // [Fact]
+        // public void TwoSum_ReturnsNegativeOneWhenNoSolution()
+        // {
+        //     // Arrange
+        //     int[] nums = { 1, 2, 3, 4 };
+        //     int target = 8;
+
+        //     // Act
+        //     int[] result = HashMaps.HashMaps.TwoSum(nums, target);
+
+        //     // Assert
+        //     Assert.Equal(new int[] { -1, -1 }, result);
+        // }
+
+        // [Fact]
+        // public void RepeatedCharacter_ReturnsFirstRepeatedCharacter()
+        // {
+        //     // Arrange
+        //     string s = "abca";
+
+        //     // Act
+        //     char result = HashMaps.HashMaps.RepeatedCharacter(s);
+
+        //     // Assert
+        //     Assert.Equal('a', result);
+        // }
+
+        // [Fact]
+        // public void RepeatedCharacter_ReturnsNullCharacterWhenNoRepetition()
+        // {
+        //     // Arrange
+        //     string s = "abcdef";
+
+        //     // Act
+        //     char result = HashMaps.HashMaps.RepeatedCharacter(s);
+
+        //     // Assert
+        //     Assert.Equal('\0', result);
+        // }
+
+        // [Fact]
+        // public void FindNumbers_ReturnsCorrectNumbers()
+        // {
+        //     // Arrange
+        //     int[] nums = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+        //     // Act
+        //     List<int> result = HashMaps.HashMaps.FindNumbers(nums);
+
+        //     // Assert
+        //     Assert.Equal(new List<int> { 1, 10 }, result);
+        // }
+    }
+}
--- code-challenges/hashing/HashMaps.cs
+++ code-challenges/hashing/HashMaps.cs
@@ -20,6 +20,57 @@ namespace CodeChallenges.HashMaps
             }
                 
             return new[] { -1, -1 };
-        }        
+        }
+
+        public static char RepeatedCharacter(string s)
+        {
+            HashSet<char> seen = new HashSet<char>();
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                char c = s[i];
+
+                if (seen.Contains(c))
+                {
+                    return c;
+                }
+
+                seen.Add(c);
+            }
+
+            return '\0';
+        }
+
+        public static List<int> FindNumbers(int[] nums)
+        {
+            List<int> ans = new List<int>();
+            HashSet<int> numSet = [.. nums];
+
+            foreach(int num in numSet)
+            {
+                if (!numSet.Contains(num + 1) && !numSet.Contains(num - 1))
+                {
+                    ans.Add(num);
+                }
+            }
+
+            return ans;
+        }
+
+        public static int MissingNumber(int[] nums)
+        {
+            int n = nums.Length;
+            HashSet<int> numSet = new HashSet<int>(nums);
+
+            for (int i = 0; i <= n; i++)
+            {
+                if (!numSet.Contains(i))
+                {
+                    return i;
+                }
+            }
+
+            return -1; // This line should never be reached if the input is valid
+        }
     }
 }
